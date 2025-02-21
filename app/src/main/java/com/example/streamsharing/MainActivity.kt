736542e8bd81816349a9c3ef2276d1e5c58c03fb
JVlazco81 <al062648@uacam.mx>
2025-02-21 12:51:40 -0600

    ajustes

--- app/src/main/java/com/example/streamsharing/MainActivity.kt
+++ app/src/main/java/com/example/streamsharing/MainActivity.kt
@@ -5,13 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.streamsharing.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Este listener ajusta el padding de la vista principal
+        // para evitar la superposición con las barras del sistema
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
